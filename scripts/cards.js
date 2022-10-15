9cbadf08eb96b4ea14e948fa0dd990a7a4fd17a4
necoop <76559254+necoop@users.noreply.github.com>
2022-10-15 20:25:04 +0300

    Add files via upload

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,49 +1,33 @@
-let cardContainer = document.querySelector(".card__container");
-// cardContainer.addEventListener("mousedown", onMouseDown);
-let cardTmp = document.querySelectorAll(".card");
-const cardWidth = document.querySelector('.card').getBoundingClientRect().width
+const cardContainer = document.querySelector(".card__container");
+const cardTmp = document.querySelectorAll(".card");
+const cardWidth = document.querySelector(".card").getBoundingClientRect().width;
+
+let currientCard = 0;
 
 let card = [];
-for(let i = 0; i < cardTmp.length; i++){
-card[i] = cardTmp[i].outerHTML;
-cardTmp[i].remove();
+for (let i = 0; i < cardTmp.length; i++) {
+  card[i] = cardTmp[i].outerHTML;
+  cardTmp[i].remove();
 }
 
-for (let i = 0; i < card.length; i++){
-  cardContainer.insertAdjacentHTML('beforeend', card[i]);
-  document.querySelectorAll('.card')[i].style.left = (10*(i+1) + cardWidth*i) + 'px';
+for (let i = 0; i < card.length; i++) {
+  cardContainer.insertAdjacentHTML("beforeend", card[i]);
+  document.querySelectorAll(".card")[i].style.left =
+    10 * (i + 1) + cardWidth * i + "px";
 }
 
 let startPositionX;
 let finishPositionX;
 let shiftX;
 
-// function onMouseDown(event) {
-//   if (event.target.closest(".card")) {
-//     cardContainer.addEventListener("mousemove", onCardMove);
-//     cardContainer.addEventListener("mouseup", onMouseUp);
-//     startPositionX = event.clientX;
-//   }
-//   function onMouseUp(event) {
-//     cardContainer.removeEventListener("mousemove", onCardMove);
-//     cardContainer.removeEventListener("mouseup", onMouseUp);
-//     finishPositionX = event.clientX;
-//   }
-//   function onCardMove(event) {
-//     event.preventDefault();
-//     shiftX = event.clientX - startPositionX;
-//     for (let i = 0; i < card.length; i++) {
-//       card[i].style.transform = "translate(" + shiftX + "px)";
-//     }
-//   }
-// }
-
 //Прокрутка на мобильных устройствах
 cardContainer.addEventListener("touchstart", onTouch);
 let startTouchPositionX;
 let cardCoordX = [];
 
-//Касание к контейнеру с картами
+// setInterval(getScrollSpeed, 100);
+
+//Касание контейнера с картами
 function onTouch(event) {
   cardContainer.addEventListener("touchend", onTouchEnd);
   startTouchPositionX = event.changedTouches[0].clientX;
@@ -51,15 +35,45 @@ function onTouch(event) {
   document.querySelector("body").style.overflowY = "hidden";
   cardContainer.addEventListener("touchmove", onTouchMove);
 
+  for (let i = 0; i < card.length; i++) {
+    document.getElementsByClassName("card")[i].style.transition = null;
+  }
+
   //Прокрутка контейнера с картами
   function onTouchMove(event) {
     let currientCoord = event.changedTouches[0].clientX;
-    let shiftX = currientCoord - startTouchPositionX;
+    shiftX = 0;
+    shiftX = currientCoord - startTouchPositionX;
     startTouchPositionX = currientCoord;
 
     for (let i = 0; i < card.length; i++) {
-      cardCoordX[i] = document.querySelectorAll('.card')[i].getBoundingClientRect().left;
-      document.querySelectorAll('.card')[i].style.left = cardCoordX[i] + shiftX + "px";
+      cardCoordX[i] = document
+        .querySelectorAll(".card")
+        [i].getBoundingClientRect().left;
+
+      //Сперва проверяем на невыход всех карт про прокрутке за пределы экрана
+      if (cardCoordX[0] + shiftX > 10) {
+        for (let i = 0; i < card.length; i++) {
+          let newCoord = (i + 1) * 10 + i * cardWidth;
+          document.querySelectorAll(".card")[i].style.left = newCoord + "px";
+        }
+      } else {
+        if (cardCoordX[card.length - 1] + shiftX < 10) {
+          for (let i = 0; i < card.length; i++) {
+            document.querySelectorAll(".card")[i].style.left =
+              (-card.length + 1 + i) * cardWidth +
+              (i - card.length + 2) * 10 +
+              "px";
+          }
+          //После этого делаем прокрутку
+        } else {
+          let newCoord = cardCoordX[i] + shiftX;
+          if (newCoord < 10 + cardWidth / 2 && newCoord > 10 - cardWidth / 2) {
+            currientCard = i;
+          }
+          document.querySelectorAll(".card")[i].style.left = newCoord + "px";
+        }
+      }
     }
   }
 
@@ -68,5 +82,12 @@ function onTouch(event) {
     cardContainer.removeEventListener("touchmove", onTouchMove);
     cardContainer.removeEventListener("touchend", onTouchEnd);
     document.querySelector("body").style.overflowY = "visible";
+
+    //Плавная прокрутка карт при отпускании
+    for (let i = 0; i < card.length; i++) {
+      document.querySelectorAll(".card")[i].style.transition = "0.8s";
+      document.querySelectorAll(".card")[i].style.left =
+        (i - currientCard) * cardWidth + 10 + (i - currientCard) * 10 + "px";
+    }
   }
 }
